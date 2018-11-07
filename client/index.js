c7cea7e886899212033d9446dda31d78d17d9f7a
Sil <silkreulen@gmail.com>
2018-11-07 11:22:22 +0100

    first commit

---
+++ client/index.js
@@ -0,0 +1,37 @@
+import './style/style.css';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Router, Route, hashHistory, IndexRoute } from 'react-router';
+import ApolloClient from 'apollo-client';
+import { ApolloProvider } from 'react-apollo';
+
+import App from './components/App';
+import SongList from './components/SongList';
+import SongCreate from './components/SongCreate';
+import SongDetail from './components/SongDetail';
+
+// Create a new instance of ApolloClient and pass it to ApolloProvider
+// We pass an empty config object => it then assumes the back-end is set-up on the route /graphql
+const client = new ApolloClient({
+  // go fetch every piece of data and use the id to identify every piece of record
+  dataIdFromObject: o => o.id
+});
+
+const Root = () => {
+  return (
+    <ApolloProvider client={client}>
+      <Router history={hashHistory}>
+        <Route path="/" component={App}>
+          <IndexRoute component={SongList}/>
+          <Route path="songs/new" component={SongCreate} />
+          <Route path="songs/:id" component={SongDetail} />
+        </Route>
+      </Router>
+    </ApolloProvider>
+  );
+};
+
+ReactDOM.render(
+  <Root />,
+  document.querySelector('#root')
+);
